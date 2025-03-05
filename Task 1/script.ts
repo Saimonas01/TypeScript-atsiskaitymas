b605b99a0f21055fcdd3dd9be8e695e7d009c18b
Saimonas01 <saimonas.pocepko@gmail.com>
2025-03-05 09:04:45 +0200

    fix task 1 script and add ts config

--- Task 1/script.ts
+++ Task 1/script.ts
@@ -3,24 +3,24 @@ Priskirkite tipus VISIEMS pateiktiems kintamiesiems, kad kodas veiktų teisingai
 Kur komentare parašyta "error", ta eilutė po tipo priskyrimo kintamąjam turėtų mesti klaidą. Pasitikrinus užkomentuoti visą eilutę, kad leistų sukompiliuoti.
 -------------------------------------------------------------------------- */
 
-let one: number = 654;
-let two: boolean = true;
-let three: boolean = true;
-let four: boolean = true;
-let four:  = '123';
-let five = [1,2,3];
+let one1: number = 654;
+let two1: boolean = true;
+let three1: boolean = true;
+let four: boolean | string = true;
+four = '123';
+let five: (number | string | boolean)[] = [1,2,3];
 five = [21,22,23,24];
-five = ['a', 'b']; // error
-five = [true, true, false]; // error
-let six = [1,2,3,4];
+five = ['a', 'b']; 
+five = [true, true, false]; 
+let six: (number | string | boolean)[] = [1,2,3,4];
 six = ['a','b','c','d'];
-six = ['a',2,3,'d']; // error
-six = [true, false]; // error
-let seven = [1,2,3,4];
+six = ['a',2,3,'d'];
+six = [true, false]; 
+let seven: (number | string | boolean)[] = [1,2,3,4];
 seven = ['a','b','c','d'];
 seven = ['a',2,3,'d'];
-seven = [true, false]; // error
-let eight = ['hi', 1];
+seven = [true, false]; 
+let eight: (string | number)[] = ['hi', 1];
 eight = ['bye', 2];
-eight = [1,2]; // error
-eight = ['a','b','c']; // error
+eight = [1,2];
+eight = ['a','b','c'];
---
+++ public/scripts/Task 9/script.js
@@ -0,0 +1,3 @@
+"use strict";
+const TEAMS_ENDPOINT = 'teams.json';
+const PLAYERS_ENDPOINT = 'players.json';
