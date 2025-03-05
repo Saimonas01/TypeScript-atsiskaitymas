c9bb7fade300b48d700486a771061d7af749ffc8
Saimonas01 <saimonas.pocepko@gmail.com>
2025-03-05 10:30:24 +0200

    add task 5

--- Task 5/script.ts
+++ Task 5/script.ts
@@ -7,3 +7,23 @@ Pvz.:
   "scissors", "rock" --> "Player 2 won!"
   "paper", "paper" --> "Draw!"
 -------------------------------------------------------------------------- */
+type Vaskinimas = "rock" | "paper" | "scissors";
+
+const zaidimas = (player1: Vaskinimas, player2: Vaskinimas): string => {
+  if (player1 === player2) {
+    return "Draw!";
+  } else if (player1 === "rock" && player2 === "scissors") {
+    return 'Player 1 laimejo';
+  } else if (player1 === "paper" && player2 === "rock") {
+    return 'Player 1 laimejo';
+  } else if (player1 === "scissors" && player2 === "paper") {
+    return 'Player 1 laimejo';
+  } else {
+    return 'Player 2 laimejo';
+  }
+};
+
+// console.log(zaidimas("scissors", "paper")); // "Player 1 laimejo"
+// console.log(zaidimas("rock", "scissors"));    // "Player 1 laimejo"
+// console.log(zaidimas("paper", "paper"));      // "Draw!"
+// console.log(zaidimas("paper", "scissors"));   // "Player 2 laimejo"
