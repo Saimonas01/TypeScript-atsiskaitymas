4adb6481bea4758050e43f8933357704609807cb
Saimonas01 <saimonas.pocepko@gmail.com>
2025-03-05 10:31:27 +0200

    task 5 tsc

--- public/scripts/Task 5/script.js
+++ public/scripts/Task 5/script.js
@@ -1,1 +1,18 @@
 "use strict";
+const zaidimas = (player1, player2) => {
+    if (player1 === player2) {
+        return "Draw!";
+    }
+    else if (player1 === "rock" && player2 === "scissors") {
+        return 'Player 1 laimejo';
+    }
+    else if (player1 === "paper" && player2 === "rock") {
+        return 'Player 1 laimejo';
+    }
+    else if (player1 === "scissors" && player2 === "paper") {
+        return 'Player 1 laimejo';
+    }
+    else {
+        return 'Player 2 laimejo';
+    }
+};
