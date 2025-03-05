a792a7af47dc73d060ed6641eb9c72060e75e623
Saimonas01 <saimonas.pocepko@gmail.com>
2025-03-05 11:33:02 +0200

    task 6 completed

--- Task 6/script.ts
+++ Task 6/script.ts
@@ -14,3 +14,38 @@ Pvz.:
   new_potion.color  =  [127, 159, 127]
   new_potion.volume =  19
 ------------------------------------------------------------------------------------------------------ */
+
+class Potion {
+  color: [number, number, number];
+  volume: number;
+
+  constructor(color: [number, number, number], volume: number) {
+    this.color = color;
+    this.volume = volume;
+    this.render();
+  }
+  
+  render(): void {
+    if (!this.color.every(subColor => subColor >= 0 && subColor <= 255)) {
+      console.error("Negalima funkcija");
+    }
+  }
+
+  mix(other: Potion): Potion {
+    const totalVolume = this.volume + other.volume;
+    const newColor: [number, number, number] = [
+      Math.round((this.color[0] * this.volume + other.color[0] * other.volume) / totalVolume),
+      Math.round((this.color[1] * this.volume + other.color[1] * other.volume) / totalVolume),
+      Math.round((this.color[2] * this.volume + other.color[2] * other.volume) / totalVolume),
+    ];
+    return new Potion(newColor, totalVolume);
+  }
+}
+
+const felix_felicis = new Potion([255, 255, 255], 7);
+const polyjuice = new Potion([51, 102, 51], 12);
+const newPotion = felix_felicis.mix(polyjuice);
+
+console.log(newPotion.color);
+console.log(newPotion.volume);
+
--- public/scripts/Task 6/script.js
+++ public/scripts/Task 6/script.js
@@ -1,1 +1,27 @@
 "use strict";
+class Potion {
+    constructor(color, volume) {
+        this.color = color;
+        this.volume = volume;
+        this.render();
+    }
+    render() {
+        if (!this.color.every(subColor => subColor >= 0 && subColor <= 255)) {
+            console.error("Negalima funkcija");
+        }
+    }
+    mix(other) {
+        const totalVolume = this.volume + other.volume;
+        const newColor = [
+            Math.round((this.color[0] * this.volume + other.color[0] * other.volume) / totalVolume),
+            Math.round((this.color[1] * this.volume + other.color[1] * other.volume) / totalVolume),
+            Math.round((this.color[2] * this.volume + other.color[2] * other.volume) / totalVolume),
+        ];
+        return new Potion(newColor, totalVolume);
+    }
+}
+const felix_felicis = new Potion([255, 255, 255], 7);
+const polyjuice = new Potion([51, 102, 51], 12);
+const newPotion = felix_felicis.mix(polyjuice);
+console.log(newPotion.color);
+console.log(newPotion.volume);
